b432d34fddbc310ec9bd62d76597ac5486f56b4a
joaogbpereira <joaopereiraudf@gmail.com>
2025-03-18 17:44:17 -0300

    ArvoreBinaria.cs

--- Listas Encadeadas/ArvoreBinaria.cs
+++ Listas Encadeadas/ArvoreBinaria.cs
@@ -1,1 +1,18 @@
+class BTreeNode
+{
+    private int valor;
+    private BTreeNode esq;
+    private BTreeNode dir;
 
+    BTreeNode(int valor)
+    {
+        this.valor = valor;
+    }
+
+    public void setValor(int valor)...
+    public void setEsq(BTreeNode esq)...
+    public void setDir(BTreeNode dir)...
+    public int getValor();
+    public BTreeNode getEsq();
+    public BTreeNode getDir();
+}
