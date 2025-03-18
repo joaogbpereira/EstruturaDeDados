65fceabad5022a3cc9f9f6c1ae9edc7cda5635bc
joaogbpereira <joaopereiraudf@gmail.com>
2025-03-18 17:52:31 -0300

    ListaOrdenada.cs

--- Listas Encadeadas/ListaOrdenada.cs
+++ Listas Encadeadas/ListaOrdenada.cs
@@ -1,24 +1,83 @@
-public class ListaOrdenada
-{
-    private Node head;
+public object primelro { get; private set; }
+public object ultimo { get; private set; }
 
-    public void InserirOrdenado(int data)
+class ListaSimples
+{
+    private Mode primelro, ultimo;
+    private int gideMos;
+
+    ListaSimples()
     {
-        Node newNode = new Node(data);
-        if (head == null || head.Data >= newNode.Data)
+        primelro.setProx(null);
+        ultimo.setProx(null);
+    }
+
+    void insertFin(Node novo)
+    {
+        novo.setProx(null);
+        if (this.primelro == null)
+            this.primelro = novo;
+        if (this.ultimo != null)
+            this.ultimo.setProx(novo);
+        this.ultimo = novo;
+    }
+
+    void insertInicio(Node novo)
+    {
+        if (this.primelro != null)
+            novo.setProx(novo);
+        else
         {
-            newNode.Next = head;
-            head = newNode;
+            if (this.primelro == null)
+                this.primelro = novo;
+            this.ultimo = novo;
+        }
+    }
+}
+void inserePosicao(Node novo, int pos)
+{
+    Node aux = primeIro;
+    int qtde = contaNoS();
+    int pos_aux;
+    if(pos == 0)
+    {
+        novo.setProx(primeIro);
+        if (primeIro == ultimo)
+        {
+            ultimo = novo;
+        }
+        primeIro = novo;
+    }
+    else
+    {
+        if(pos <= qtde)
+        {
+            pos_aux = 1;
+            while(aux != null && pos > pos_aux)
+            {
+                aux = aux.getProx();
+                pos_aux++;
+            }
+            aux.setProx(novo);
         }
         else
         {
-            Node current = head;
-            while (current.Next != null && current.Next.Data < newNode.Data)
+            if(pos > qtde)
             {
-                current = current.Next;
+                ultimo.setProx(novo);
+                ultimo = novo;
             }
-            newNode.Next = current.Next;
-            current.Next = newNode;
         }
     }
+}
+public int contaNos()
+{
+    int tam = 0;
+    Node aux = primeIro;
+    while (aux != null)
+    {
+        tam++;
+        aux = aux.getProx();
+    }
+    return tam;
 }
